8ce661b606bd53a5faaeb6b5acdd24e27374b02c
Luke Ward <luke__ward@hotmail.co.uk>
2016-01-25 14:44:38 +0000

    Added Report class

---
+++ CSSD-Agriculture-D/Project/CSSDFarm/src/CSSDFarm/Report.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CSSDFarm;
+import java.util.*;
+
+/**
+ *
+ * @author Luke
+ */
+public class Report {
+    
+    private Map<String, HistoricalData> data;
+    
+    public Vector<SensorData> getDataByDate(Date){
+        return null;
+    }
+    public Vector<HistoricalData> getDataByType(String){
+        return null;
+    }
+    
+    public Vector<SensorData> getDataByTypeAndDate(String, Date){
+        return null;
+    }
+    
+    public void addData(SensorData){
+        return null;
+    } 
+    
+}
