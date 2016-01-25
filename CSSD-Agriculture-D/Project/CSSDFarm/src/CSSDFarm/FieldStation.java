68987076d4bb8ad12b662da4a547f6debbd21249
Luke Segaran <lnsegaran@gmail.com>
2016-01-25 14:43:48 +0000

    Added FieldStation and UserAccount

---
+++ CSSD-Agriculture-D/Project/CSSDFarm/src/CSSDFarm/FieldStation.java
@@ -0,0 +1,46 @@
+package CSSDFarm;
+
+import java.util.*;
+
+public class FieldStation {
+    private String name;
+    private String id;
+    private Vector<Sensor> sensors;
+    private Vector<SensorData> buffer;
+    
+    public Report compileReport(){
+        return null;
+    }
+    
+    public void addSensor(Sensor sensor){
+        
+    }
+    
+    public void removeSensor(String id){
+        
+    }
+    
+    public void update(String string1, SensorData sensorData){
+        
+    }
+    
+    public SensorData getData(String id){
+        return null;
+    }
+    
+    public boolean uploadData(){
+        return null;
+    }
+    
+    public void addToBuffer(String string1, SensorData sensorData){
+        
+    }
+    
+    public void clearBuffer(){
+        
+    }
+    
+    public void addSensor(String string1, String string2, String string3){
+        
+    }
+}
---
+++ CSSD-Agriculture-D/Project/CSSDFarm/src/CSSDFarm/UserAccount.java
@@ -0,0 +1,33 @@
+package CSSDFarm;
+import java.util.*;
+
+public class UserAccount {
+    private Vector<FieldStation> stations;
+    private String username;
+    private String password;
+    
+    public boolean CheckCredentials(String username, String password){
+        return null;
+    }
+    
+    public void AddStation(FieldStation fieldStation){
+        
+    }
+    
+    public void RemoveStation(FieldStation fieldStation){
+        
+    }
+    
+    public FieldStation GetStation(String id){
+        return null;
+    }
+    
+    public Vector<FieldStation> getFieldStations(){
+        return null;
+    }
+    
+    public boolean canAccess(String string1){
+        return null;
+    }
+    
+}
