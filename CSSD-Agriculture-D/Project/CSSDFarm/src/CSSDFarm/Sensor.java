045eb2779c27f968ddcd401aad67e6cb8c733918
Luke Ward <luke__ward@hotmail.co.uk>
2016-01-26 16:13:33 +0000

    Sensor changes.

--- CSSD-Agriculture-D/Project/CSSDFarm/src/CSSDFarm/Sensor.java
+++ CSSD-Agriculture-D/Project/CSSDFarm/src/CSSDFarm/Sensor.java
@@ -13,27 +13,44 @@ public class Sensor {
     private float threshold;
     private boolean thresholdIsUpperLimit;
     private Date lastReadingTime;
-    private SensorData sensorData;
+    private Actuator actuator;
     
     public SensorData getData()
     {   
-        return null;
+        return data;
     }
     
     public void toggleModule()
     {
+        if(actuator.isActive())
+        {
+            actuator.deactivate();
+        }
+        else
+        {
+            actuator.activate();
+        }
     }
     
     public void collectData()
     {
+        //Replace 'mm' with 'unit'
+        //Replace 12.0f with 'value'
+        data = new SensorData(id, new Date(),"mm",12.0f, location);
     }
     
     public void onInterval()
     {
+        //If lastReadingTime == lastReadingTime + intervalSeconds
+        /*if(new Date(lastReadingTime.getSeconds() + intervalSeconds) == new Date())
+        {
+            collectData();
+        }*/
     }
     
     public void setFieldStation(FieldStation fieldStation)
     {
+        station = fieldStation;
     }
     
     public void activate()
@@ -42,6 +59,8 @@ public class Sensor {
     
     public void calculateLocation()     
     {
+        //change to realistic gps data
+        location = new GPSData(12345.54321f, 6789.9876f, 123.4f);
     }
     
     public void update(FieldStation fieldStation, Sensor sensor)
@@ -50,6 +69,27 @@ public class Sensor {
     
     public boolean withinThreshold()
     {
-        return false;
+        if(thresholdIsUpperLimit)
+        {
+            if(data.getValue() <= threshold)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            if(data.getValue() >= threshold)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
