6cc3a7876b71148fabf52ff901fe9164dedc76ba
Kodah <tom_1440@hotmail.co.uk>
2016-02-24 17:12:12 +0000

    Override toString

--- CSSD-Agriculture-D/Project/CSSDFarm/src/CSSDFarm/GPSData.java
+++ CSSD-Agriculture-D/Project/CSSDFarm/src/CSSDFarm/GPSData.java
@@ -1,29 +1,35 @@
 package CSSDFarm;
 
 public class GPSData {
+
     private float latitude;
     private float longitude;
     private float altitude;
-    
-    public GPSData(float latitude, float longitude, float altitude){
+
+    public GPSData(float latitude, float longitude, float altitude) {
         this.latitude = latitude;
         this.longitude = longitude;
         this.altitude = altitude;
     }
-    
-    public float getLatitude(){
+
+    public float getLatitude() {
         return latitude;
     }
-    
-    public float getLongitude(){
+
+    public float getLongitude() {
         return longitude;
     }
-    
-    public float getAltitude(){
+
+    public float getAltitude() {
         return altitude;
     }
-        
-    public String GPStoString(){
+
+    @Override
+    public String toString() {
+        return "{lat : " + this.latitude + ", long : " + this.longitude + ",alt : " + this.altitude + "}";
+    }
+
+    public String GPStoString() {
         return latitude + "," + longitude + "," + altitude;
     }
 }
