9d62dfd5675fa0e08f24327a9f578cec0ca0267b
Luke <luke__ward@hotmail.co.uk>
2016-03-04 21:08:39 +0000

    Added javadocs for Actuator and GPSData.

--- CSSD-Agriculture-D/Project/CSSDFarm/src/CSSDFarm/Actuator.java
+++ CSSD-Agriculture-D/Project/CSSDFarm/src/CSSDFarm/Actuator.java
@@ -3,27 +3,49 @@ package CSSDFarm;
 import java.io.Serializable;
 
 /**
-* The Actuator system that interacts with Sensors
+* The Actuator is the mechanism attached to sensors that, when active, aims to 
+* increase or decrease the sensors data reading through a mechanical action. e.g. turning on sprinkler
 */
 
 public class Actuator implements Serializable{
     private boolean isActive;
     
+    /**
+     * Default constructor for Actuator 
+     */
     public Actuator(){
     }
     
+    /**
+     * Constructor for Actuator class.
+     * The supplied boolean sets the Actuators isActive attribute upon creation
+     *
+     * @param isActive boolean determining whether the actuator is On or Off
+     */
     public Actuator(boolean isActive){
         this.isActive = isActive;
     }
     
+    /**
+     * Sets the Actuators isActive attribute to true 
+     * meaning the Actuator is currently active.
+     */
     public void activate(){
         isActive = true;
     }
     
+    /**
+     * Gets the isAtive boolean determining whether the Actuator is on or off.
+     * @return The isActive boolean, whether the Actuator is currently on or off
+     */
     public boolean isActive(){        
         return isActive;
     }
     
+    /**
+     * Sets the Actuator isActive attribute to false
+     * meaning the Actuator is currently not active.
+     */
     public void deactivate(){
         isActive = false;
     }
--- CSSD-Agriculture-D/Project/CSSDFarm/src/CSSDFarm/GPSData.java
+++ CSSD-Agriculture-D/Project/CSSDFarm/src/CSSDFarm/GPSData.java
@@ -2,25 +2,49 @@ package CSSDFarm;
 
 import java.io.Serializable;
 
+/**
+ * GPSData contains the sensors current geographical coordinates
+ * @author Luke
+ */
 public class GPSData implements Serializable{
     private float latitude;
     private float longitude;
     private float altitude;
     
+    /**
+     * GPSData Constructor takes the current GPS latitude, longitude and altitude 
+     * and sets the relevant attributes
+     * 
+     * @param latitude A float representing the latitude geographic coordinate of the GPS
+     * @param longitude A float representing the longitude geographic coordinate of the GPS
+     * @param altitude A float representing the altitude of the GPS
+     */
     public GPSData(float latitude, float longitude, float altitude){
         this.latitude = latitude;
         this.longitude = longitude;
         this.altitude = altitude;
     }
     
+    /**
+     * Gets the GPS latitude value
+     * @return The current GPS latitude float
+     */
     public float getLatitude(){
         return latitude;
     }
     
+    /**
+     * Gets the GPS longitude value
+     * @return The current GPS longitude float
+     */
     public float getLongitude(){
         return longitude;
     }
     
+    /**
+     * Gets the GPS altitude value
+     * @return The current GPS altitude float
+     */
     public float getAltitude(){
         return altitude;
     }
@@ -30,6 +54,10 @@ public class GPSData implements Serializable{
         return "{lat : " + this.latitude + ", long : " + this.longitude + ",alt : " + this.altitude + "}";
     }
         
+    /**
+     * The GPS latitude, longitude and altitude formatted to a geographic coordinate string
+     * @return The GPS latitude, longitude and altitude string
+     */
     public String GPStoString(){
         return latitude + "," + longitude + "," + altitude;
     }
