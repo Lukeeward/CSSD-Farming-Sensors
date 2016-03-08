bde44ee77fab473f099e2814b1e7cb2be55ff22a
TehWebby <shaunwebbit@gmail.com>
2016-03-08 12:13:47 +0000

    test automation

---
+++ CSSD-Agriculture-D/Project/CSSDFarm/Tests/UserInterfaceTests.java
@@ -0,0 +1,278 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import CSSDFarm.FieldStation;
+import CSSDFarm.Server;
+import CSSDFarm.UserInterface;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.ClipboardOwner;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import static java.awt.event.KeyEvent.*;
+import java.util.Random;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Webby
+ */
+public class UserInterfaceTests {
+    //static final Server server = Server.getInstance();
+    UserInterface ui = new UserInterface();
+    Robot bot;
+    ClipboardOwner owner = new ClipboardOwner() {
+        public void lostOwnership(Clipboard clipboard,Transferable contents){}
+    };    
+    Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+    StringSelection stringSelection;
+    
+    public UserInterfaceTests() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        try{
+            bot = new Robot();
+        }
+        catch(Exception eX){
+            System.out.println(eX);
+        }
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testGUI(){
+        ui.setVisible(true);
+        
+        //login
+        logIn();
+        goToReportScreen();
+        
+        removeAllFieldStations();
+        //assertTrue(server.getFieldStation("") == null);
+        
+        for (int i=0;i<2;i++){
+            addFieldStation();
+            
+            for (int l=0;l<2;l++)
+                addSensor();
+        }
+        
+        //FieldStation result = server.getFieldStation("");
+        //assertTrue(result != null);
+        bot.mouseMove(ui.btnReport.getLocationOnScreen().x,ui.btnReport.getLocationOnScreen().y);
+        clickMouse();
+        waitForGui();
+        
+        //assertTrue(ui.listUserStations.getModel().getElementAt(0).toString() != null);
+        
+        
+        //removeAllFieldStations();
+        
+        //removeFieldStation();
+        
+        
+        try {
+            Thread.sleep(5000);                 //1000 milliseconds is one second.
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+        
+        
+        
+    }
+    
+    public void logIn(){
+        //enters username and password on GUI
+        selectAll();
+        pasteString("John");
+        tabKey();
+        selectAll();
+        pasteString("Password");
+        tabKey();
+        enterKey();
+    }
+    
+    public void pasteString(String paste){
+        stringSelection = new StringSelection(paste);
+        clipboard.setContents(stringSelection, owner);
+        bot.keyPress(KeyEvent.VK_CONTROL);
+        bot.keyPress(KeyEvent.VK_V);
+        bot.keyRelease(KeyEvent.VK_V);
+        bot.keyRelease(KeyEvent.VK_CONTROL);
+        waitForGui();
+        
+    }
+    
+    public void clickMouse(){
+        waitForGui();
+        bot.mousePress(InputEvent.BUTTON1_MASK);
+        bot.mouseRelease(InputEvent.BUTTON1_MASK);
+        waitForGui();
+    }
+    
+    public void enterKey(){
+        waitForGui();
+        bot.keyPress(KeyEvent.VK_ENTER);
+        bot.keyRelease(KeyEvent.VK_ENTER);
+        waitForGui();
+    }
+    
+    public void goToReportScreen(){
+        bot.mouseMove(ui.btnManager.getLocationOnScreen().x,ui.btnManager.getLocationOnScreen().y);
+        clickMouse();
+    }
+    
+    public void addFieldStation(){
+        bot.mouseMove(ui.btnAddFieldStation.getLocationOnScreen().x,ui.btnAddFieldStation.getLocationOnScreen().y);
+        clickMouse();
+        int n = genRanNum(50);
+        pasteString("FieldStationTest" + n);
+        tabKey();
+        pasteString("FieldStationTest" + n);
+        tabKey();
+        spaceKey();
+        waitForGui();
+        String fname = ui.listUserStations.getSelectedValue().toString();
+        System.out.println(fname);
+    }
+    
+    public void addSensor(){
+        bot.mouseMove(ui.btnAddSensor.getLocationOnScreen().x,ui.btnAddSensor.getLocationOnScreen().y);
+        clickMouse();
+        waitForGui();
+        int n = genRanNum(1500);
+        ui.txtSensorId.setText("SensorTest" + n);
+        tabKey();
+        selectSensorType();
+        selectSensorUnits();
+        selectSensorIntervalSecs();
+        selectThreshold();
+        selectIfUpperLimit();
+    }
+    
+    public int genRanNum(int limit){
+        Random rand = new Random();
+        return rand.nextInt(limit) + 1;
+    }
+    
+    public void removeFieldStation(){
+        waitForGui();
+        bot.mouseMove(ui.btnRemoveFieldStation.getLocationOnScreen().x,ui.btnRemoveFieldStation.getLocationOnScreen().y);
+        clickMouse();
+        spaceKey();
+    }
+    
+    public void selectSensorType(){
+        //selects the sensor type 
+        int totalSTypes = genRanNum(ui.comboSensorType.getItemCount());
+        System.out.println(totalSTypes);
+        for (int l=0;l<totalSTypes;l++)
+            downKey();
+        enterKey();
+        tabKey();
+    }
+    
+    public void selectSensorUnits(){
+        int totalSUnits = genRanNum(ui.comboSensorUnits.getItemCount());
+        for (int i=0;i<totalSUnits;i++)
+            downKey();
+        enterKey();
+        tabKey();
+        tabKey();
+        tabKey();
+        tabKey();
+    }
+    
+    public void selectThreshold(){
+        pasteString(String.valueOf(genRanNum(150)));
+        tabKey();
+    }
+    
+    public void selectIfUpperLimit(){
+        int total = genRanNum(1);
+        for (int i=0;i<total;i++)
+            spaceKey();
+        bot.mouseMove(ui.btnSaveSensor.getLocationOnScreen().x,ui.btnSaveSensor.getLocationOnScreen().y);
+        clickMouse();
+    }
+    
+    private void selectSensorIntervalSecs() {
+        int totalS = genRanNum(10);
+        if (totalS == 0)
+            totalS = 2;
+        for (int i=0;i<totalS;i++)
+            downKey();
+        tabKey();
+    }
+    
+    public void removeAllFieldStations(){
+        waitForGui();
+        int totalFieldStations = ui.listUserStations.getModel().getSize();
+        System.out.println(totalFieldStations);
+        for (int i=0;i<totalFieldStations;i++)
+            removeFieldStation();
+    }
+    
+    public void spaceKey(){
+        waitForGui();
+        bot.keyPress(KeyEvent.VK_SPACE);
+        bot.keyRelease(KeyEvent.VK_SPACE);
+        waitForGui();
+    }
+    
+    public void selectAll(){
+        waitForGui();
+        bot.keyPress(KeyEvent.VK_CONTROL);
+        bot.keyPress(KeyEvent.VK_A);
+        waitForGui();
+    }
+    
+    public void tabKey(){
+        waitForGui();
+        bot.keyPress(KeyEvent.VK_TAB);
+        bot.keyRelease(KeyEvent.VK_TAB);
+        waitForGui();
+    }
+    
+    public void downKey(){
+        waitForGui();
+        bot.keyPress(KeyEvent.VK_DOWN);
+        bot.keyRelease(KeyEvent.VK_DOWN);
+    }
+    
+    public void waitForGui(){
+        try {
+            Thread.sleep(250);                 
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    
+    
+    
+}
